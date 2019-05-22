12379d9db4c1c58eea873dde4ecf0faf308c561b
invano <cozziemanuele@gmail.com>
2019-05-22 10:58:23 +0200

    idapython: add print_insn_mnem, print_operand, get_operand_type,    find_func_end but mantain previous name for API compatibility idapython: add old API name get_long

--- idb/idapython.py
+++ idb/idapython.py
@@ -756,6 +756,12 @@ class idc:
         else:
             raise RuntimeError('unexpected wordsize')
 
+        ## Mantain API compatibility for API < 7
+        self.GetMnem = self.print_insn_mnem
+        self.GetOpnd = self.print_operand
+        self.GetOpType = self.get_operand_type
+        self.FindFuncEnd = self.find_func_end
+
     def ScreenEA(self):
         return self.api.ScreenEA
 
@@ -962,7 +968,7 @@ class idc:
         else:
             return op
 
-    def GetMnem(self, ea):
+    def print_insn_mnem(self, ea):
         op = self._disassemble(ea)
         return op.mnemonic
 
@@ -970,7 +976,7 @@ class idc:
         op = self._disassemble(ea)
         return '%s\t%s' % (op.mnemonic, op.op_str)
 
-    def GetOpnd(self, ea, n):
+    def print_operand(self, ea, n):
         op = self._disassemble(ea)
         opnds = op.op_str.split(", ")
         n_opnds = len(opnds)
@@ -980,7 +986,7 @@ class idc:
         else:
             return ""
 
-    def GetOpType(self, ea, n):
+    def get_operand_type(self, ea, n):
         from capstone import CS_OP_INVALID, CS_OP_REG, CS_OP_MEM, CS_OP_IMM
 
         op = self._disassemble(ea)
@@ -1082,7 +1088,7 @@ class idc:
     def GetFunctionName(self, ea):
         return self.api.ida_funcs.get_func_name(ea)
 
-    def FindFuncEnd(self, ea):
+    def find_func_end(self, ea):
         func = self.api.ida_funcs.get_func(ea)
         if not func:
             return self.BADADDR
@@ -1261,6 +1267,9 @@ class ida_bytes:
     def __init__(self, db, api):
         self.idb = db
         self.api = api
+
+        ## Mantain API compatibility for API < 7
+        self.get_long = self.get_dword
 
     def get_cmt(self, ea, repeatable):
         flags = self.api.idc.GetFlags(ea)
